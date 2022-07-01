5c7e0d2a13a68bc42ce2852e6c9e59fd7209c4a2
Yanique Sears <yaniquesears@gmail.com>
2022-07-01 01:20:04 +0000

    Added comments

--- programming/1/Nim.java
+++ programming/1/Nim.java
@@ -15,32 +15,36 @@ public static void main(String [] args)
     int stonesTaken;
 
     Scanner input = new Scanner(System.in);
-      Random rand = new Random();
+      Random rand = new Random(); //random number for AI
     while(stones > 0 ){
         System.out.print("How many stones do you want to take first? 1-3 ");
-      stonesTaken = input.nextInt();
+      stonesTaken = input.nextInt(); //user's answer stored here
       while (stonesTaken > 3){
         System.out.print("Pick 1-3 stones please!");
         stonesTaken = input.nextInt();
       }
-      stones -= stonesTaken; 
+      stones -= stonesTaken; // calculates # of stones remaining
     if (stones <= 0) {
         System.out.println ("Player wins");
       break;
     }
-        System.out.println(stones + " Remaining");
+        System.out.println(stones + " Remaining"); //will print # of stones remaining
+      
     //Computer turn
-        stonesTaken= rand.nextInt(3)+1;
+        stonesTaken= rand.nextInt(3)+1; //+1 accounts for #3
       System.out.println( "The computer took "+ stonesTaken + " stones");
       if (stonesTaken>stones) {
-        stonesTaken=stones;
+        stonesTaken=stones; //if there are more stones taken than are left, the # will be set equal to whatever is left
       }
-        stones -= stonesTaken;
+        stones -= stonesTaken; // will calculate stones remaining
         if(stones <=0) {
         System.out.println("Computer wins");
           break;
       }
         System.out.println(stones + " Remaining");
+
+
+      
       // prompt user input (user turn)
 
       // calculate number of stone remaining, print
