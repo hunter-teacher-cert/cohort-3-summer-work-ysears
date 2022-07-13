e4e8ffcc5afa12cb13d4f2db5c2f9cf3867e72ab
Yanique Sears <yaniquesears@gmail.com>
2022-07-13 17:48:53 +0000

    updated node assignment

---
+++ ds/pointers/node part 3.java
@@ -0,0 +1,9 @@
+/*
+Write a code fragment to:
+Create a new Node variable set it to point to the node with the 10 in it.
+Create a new Node variable and instantiate it to a new Node with a value of 30.
+Write the code to insert this new Node between the 10 and the 15
+*/
+
+Node n1 = n1.setNext(n2)
+Node n1 = new Node(10,n1);
